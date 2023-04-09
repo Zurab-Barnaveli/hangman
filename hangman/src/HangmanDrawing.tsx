9300899491f656a838e5c41ddfb8f47a88a7098d
Zurab-Barnaveli <zuka.barnaveli11@gmail.com>
2023-04-09 18:01:04 +0400

    first commit

---
+++ hangman/src/HangmanDrawing.tsx
@@ -0,0 +1,127 @@
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+);
+
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "120px",
+      right: 0,
+    }}
+  />
+);
+
+const Right_Arm = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "-100px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+
+const Left_Arm = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+
+const Right_Leg = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: "-90px",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+
+const Left_Leg = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: "0px",
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+
+const Body_Parts = [HEAD, BODY, Right_Arm, Left_Arm, Right_Leg, Left_Leg];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+  return (
+    <div style={{ position: "relative" }}>
+      {Body_Parts.slice(0, numberOfGuesses)}
+      <div
+        style={{
+          height: "50px",
+          width: "10px",
+          background: "black",
+          position: "absolute",
+          top: 0,
+          right: 0,
+        }}
+      />
+      <div
+        style={{
+          height: "10px",
+          width: "200px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div style={{ height: "10px", width: "250px", background: "black" }} />
+    </div>
+  );
+}
